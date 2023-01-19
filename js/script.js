1dd2a081a6f309f3421e0e9fda3a0380f07db88c
yurapasko <98215040+yurapasko@users.noreply.github.com>
2023-01-19 13:23:39 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.about_each02').slick({
+      autoplay: false,
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          autoplay: false,
+        }
+      }]
+    });
+  });
+
+  $(document).ready(function() {
+    $('.about_each2').slick({
+      autoplay: false,
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          autoplay: false,
+        }
+      }]
+    });
+  });
